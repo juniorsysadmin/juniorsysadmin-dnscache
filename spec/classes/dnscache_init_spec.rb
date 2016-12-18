a9b8d4fb0af922b1660f91a2df90e1cad3695dfa
juniorsysadmin <juniorsysadmin@users.noreply.github.com>
2016-12-18 15:55:32 +1100

    Add unit test actions

--- spec/classes/dnscache_init_spec.rb
+++ spec/classes/dnscache_init_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe 'dnscache' do
+describe 'dnscache', type: :class do
+  context 'Fedora with no parameters, 64 bit' do
+    let(:facts) { { osfamily: 'RedHat', operatingsystem: 'Fedora', architecture: 'x86_64' } }
+    it { is_expected.to compile.with_all_deps }
+  end
 
+  context 'Ubuntu with no parameters' do
+    let(:facts) { { lsbdistid: 'ubuntu', lsbdistcodename: 'trusty', lsbdistrelease: '14.04', osfamily: 'Debian', architecture: 'x86_64' } }
+    it { is_expected.to compile.with_all_deps }
+  end
 end
